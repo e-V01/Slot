c6765e4ec02c18a4cf9f10ae29a6d6006dadedb1
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-03-20 12:40:48 +0200

    Initial Commit

---
+++ Slot/SlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotApp.swift
+//  Slot
+//
+//  Created by Y K on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
